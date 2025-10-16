93694c3ebba6087b160263d6f3f7f0ed214998f4
Aayobami001 <aakinbiyi@byupathway.edu>
2025-10-16 16:31:13 -0600

    Javascrip additional for footer

--- project/scripts/contact.js
+++ project/scripts/contact.js
@@ -182,3 +182,11 @@
 
   document.addEventListener("DOMContentLoaded", init);
 })();
+
+// Get the current year and display it in the footer
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get the last modified date and display it in the footer
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
